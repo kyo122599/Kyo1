c29d21f3fb195d0334906ab59c712fd1acd47b4e
kyo122599 <brayan36alexander@gmail.com>
2024-11-21 21:59:23 -0600

    se agrega el proyecto

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const AppManager= require('./src/componets/AppManager');
-
-const run=async()=>{
-const oAppManager=new AppManager();
-await oAppManager.prepareService();
-await oAppManager.runService();
-
-}
-run();
